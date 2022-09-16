8ed68bb9779d61721f719c04ae2bd3a9e6324a82
Danilo1dev <danilofoxlog@gmail.com>
2022-09-15 23:09:54 -0300

    inicia projeto

---
+++ src/test/java/br/com/exactaworks/gestaodespesas/GestaoDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exactaworks.gestaodespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
